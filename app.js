3c971db4a31c2318c71f0e63d43098037ae3b585
Rakmo33 <omkardabir2013@gmail.com>
2020-08-16 13:40:15 +0530

    removed seeds from app.js

--- app.js
+++ app.js
@@ -14,12 +14,9 @@ var mongoose = require("mongoose")
 var User = require("./models/user")
 var Campground = require("./models/campground")
 var Comment = require("./models/comments")
-// var seedDB = require("./seeds")
 
 flash = require("connect-flash")
 app.locals.moment = require('moment');
-
-// seedDB();
 
 //? Requiring Routes
 var campgroundRoutes = require("./routes/campgrounds")
